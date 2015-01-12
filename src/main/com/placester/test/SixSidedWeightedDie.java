8ad0ba0fd5e4e9ae73def6523bcbacd1bd666c3f
vsluchak <vsluchak@gmail.com>
2015-01-12 17:19:09 -0500

    Update SixSidedWeightedDie.java
    In the works

--- src/main/com/placester/test/SixSidedWeightedDie.java
+++ src/main/com/placester/test/SixSidedWeightedDie.java
@@ -13,9 +13,25 @@ public class SixSidedWeightedDie extends WeightedDie
 {
     //NOTE: since these are weights on a probability distribution, these should sum to one, and the incoming array
     // should be of length 6. You should throw if either of these preconditions is false
+    private int[] sidesSet;
     public SixSidedWeightedDie(float[] weights)
     {
         super(weights);
+        try {
+            if (weights.length) != 6 throw new IIOException("The number of weights should be 6, please fix.");
+            float sum = 0.0;
+            for (float i : weight) sum += sum;
+            if (sum != 1.0) throw new IIOException("The sum of weights should be 1.0, please fix.")
+        
+            
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
+        
+        
+    }
         // TODO Auto-generated constructor stub
     }
 
